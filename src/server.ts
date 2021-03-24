328ae55e49a561b49469a07fd0fadc72c73f7ef3
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2021-03-24 00:10:14 -0300

    Alterando rotas

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,29 @@ const app = express();
 
 const port = 3333;
 
-app.get('/.well-know/assets', (req, res) => {
-  return res.json([{ message: 'Hello World!' }]);
+app.get('/.well-know/assetslinks.json', (req, res) => {
+  return res.json([{
+    "relation": ["delegate_permission/common.handle_all_urls"],
+    "target": {
+      "namespace": "android_app",
+      "package_name": "com.example",
+      "sha256_cert_fingerprints":
+      ["14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5"]
+    }
+  }]);
 });
 
-app.get('/.well-know/apple', (req, res) => {
-  return res.json([{ message: 'Hello World!' }]);
+app.get('/.well-know/apple-app-site-association', (req, res) => {
+  return res.json({
+    "applinks": {
+      "apps": [],
+      "details": [{
+        "appID": "D3KQX62K1A.com.example.deeplinkexample",
+        "paths": ["/home"]
+        },
+      ]
+    }
+  });
 });
 
 app.listen(port, () => {
